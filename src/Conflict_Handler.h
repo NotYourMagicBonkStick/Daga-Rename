49d53e7ee8b6c8e9302f3d4ac06b7686048dab53
Alexander Yell <159653865+NotYourMagicBonkStick@users.noreply.github.com>
2024-06-09 20:39:56 +0200

    fix: messed up filenames on conflicts with no ID

---
+++ src/Conflict_Handler.h
@@ -0,0 +1,40 @@
+// Licensed under GNU GPL 3.0
+// Author Alexander Yell "https://mastodon.nove.team/@NotYourMagicBonkStick"
+
+
+#include <filesystem>
+#include <string>
+
+#include "Get_Extension.h"
+
+std::filesystem::path Conflict_Handler(const std::filesystem::path& inDir, const std::string& ID, int i = 1) {
+
+    std::filesystem::path outDir = inDir;
+
+    if (std::filesystem::exists(inDir)) {
+
+        std::string procString;
+        procString = inDir.string();
+
+
+        // Prevents presence of ID from messing up the filename
+        if("" == ID){
+            procString.insert(procString.length() - Get_Extension(inDir).length(), '-' + std::to_string(i));
+        }else{
+            procString.insert(procString.length() - Get_Extension(inDir).length() - ID.length() -1, '-' + std::to_string(i));
+        }
+
+
+        outDir = std::filesystem::path(procString);
+
+
+        if (std::filesystem::exists(outDir)) {
+            outDir = Conflict_Handler(inDir, ID, i + 1);
+        }
+
+    }
+
+    return outDir;
+}
+
+// yyyy-mm-dd_hh-mm-ss.ext
